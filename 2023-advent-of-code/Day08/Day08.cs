e12b08dcb0fd090f663851504b8971ec2c6ae946
David Gómez <ldavid.gomez@gmail.com>
2023-12-08 16:03:21 +0100

    Refactor

--- 2023-advent-of-code/Day08/Day08.cs
+++ 2023-advent-of-code/Day08/Day08.cs
@@ -5,37 +5,37 @@ namespace _2023_advent_of_code.Day08;
 
 public class Day08
 {
+    #region Fields
+
     private readonly string[] _input;
     private ImmutableList<char> _instructions;
     private readonly Dictionary<string, (string, string)> _map = new();
     private readonly Dictionary<string, (string, int)> _knownPositions = new();
 
+    #endregion
+
+    #region Constructors
+
     public Day08(string path)
     {
-        _input = File.ReadAllLines(path).ToArray();
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            throw new ArgumentException("Invalid file path.");
+        }
+
+        _input = File.ReadAllLines(path);
         SetMap();
     }
 
     public Day08(string[] input)
     {
-        _input = input;
+        _input = input ?? throw new ArgumentNullException(nameof(input));
         SetMap();
     }
 
-    private void SetMap()
-    {
-        _instructions = _input[0].ToImmutableList();
+    #endregion
 
-        var list = _input[1..].Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
-        list.ForEach(x =>
-        {
-            var keyValuePair = x.Replace("(", "").Replace(")", "").Split("=");
-            var key = keyValuePair[0].Trim();
-            var values = keyValuePair[1].Split(",").Select(v => v.Trim()).ToArray();
-            var value = (values[0], values[1]);
-            _map.Add(key, value);
-        });
-    }
+    #region Public Methods
 
     public long SolvePart1()
     {
@@ -53,23 +53,46 @@ public class Day08
 
     public long SolvePart2()
     {
-        var steps = new List<long>();
-
-        var entries = _map.Keys.Where(x => x.Last() == 'A').ToList();
-        foreach (var entry in entries)
-        {
-            var step = 0L;
-            var nextPosition = entry;
-
-            while (nextPosition.Last().ToString() != "Z")
-            {
-                (nextPosition, var outputSteps) = SolveMap(nextPosition, 0);
-                step += outputSteps;
-            }
-            steps.Add(step);
-        }
+        var steps = _map.Keys
+            .Where(x => x.Last() == 'A')
+            .Select(SolveMapSteps)
+            .ToList();
 
         return LeastCommonMultiple(steps);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void SetMap()
+    {
+        _instructions = _input[0].ToImmutableList();
+
+        var list = _input[1..].Where(x => !string.IsNullOrWhiteSpace(x));
+
+        foreach (var x in list)
+        {
+            var keyValuePair = x.Replace("(", "").Replace(")", "").Split("=");
+            var key = keyValuePair[0].Trim();
+            var values = keyValuePair[1].Split(",").Select(v => v.Trim()).ToArray();
+            var value = (values[0], values[1]);
+            _map.Add(key, value);
+        }
+    }
+
+    private long SolveMapSteps(string entry)
+    {
+        var step = 0L;
+        var nextPosition = entry;
+
+        while (nextPosition.Last().ToString() != "Z")
+        {
+            (nextPosition, var outputSteps) = SolveMap(nextPosition, 0);
+            step += outputSteps;
+        }
+
+        return step;
     }
 
     private static long GreatestCommonDivisor(long a, long b)
@@ -93,6 +116,7 @@ public class Day08
         var outputPosition = string.Empty;
         var nextPosition = inputPosition;
         var key = string.Empty;
+
         foreach (var instruction in _instructions)
         {
             if (firstInstruction)
@@ -119,11 +143,12 @@ public class Day08
                 return (outputPosition, step);
 
             nextPosition = outputPosition;
-
         }
 
         _knownPositions.TryAdd(key, (outputPosition, step));
 
         return (outputPosition, step);
     }
+
+    #endregion
 }
